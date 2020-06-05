1a5aae691de0a1ae3e3380591558c7407e2b7b4c
Daniel Imfeld <daniel@danielimfeld.com>
2020-06-05 22:00:32 +0000

    sharing widgets

--- src/pages/index.svelte
+++ src/pages/index.svelte
@@ -21,10 +21,32 @@
 </script>
 
 <div class="flex flex-col">
+  <div class="flex sm:hidden justify-end items-end space-x-2 px-2">
+    <div
+      class="fb-share-button"
+      data-href="https://fundtherebuild.com"
+      data-layout="button_count"
+      data-size="small">
+      <a
+        target="_blank"
+        href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Ffundtherebuild.com%2F&amp;src=sdkpreparse"
+        class="fb-xfbml-parse-ignore">
+        Share
+      </a>
+    </div>
+    <a
+      href="https://twitter.com/share?ref_src=twsrc%5Etfw"
+      class="twitter-share-button"
+      data-url="https://fund-the-rebuild.now.sh"
+      data-show-count="false">
+      Tweet
+    </a>
+
+  </div>
   <div
     class="flex flex-col space-y-2 px-2 sm:px-0 sm:space-y-0 sm:flex-row text-sm
-    text-gray-700 font-medium sm:space-x-4">
-    <div class="flex flex-col">
+    text-gray-700 font-medium sm:items-end">
+    <div class="flex flex-col sm:ml-4">
       <span>Region</span>
       <select
         class="text-base sm:text-sm text-gray-700 font-medium mt-1 form-select"
@@ -38,7 +60,7 @@
       </select>
     </div>
 
-    <div class="flex flex-col">
+    <div class="flex flex-col sm:ml-4">
       <span class="text-sm text-gray-700 font-medium">Show First</span>
       <select
         class="text-base sm:text-sm text-gray-700 font-medium mt-1 form-select"
@@ -52,6 +74,32 @@
 
       </select>
     </div>
+
+    <div class="hidden sm:ml-auto sm:flex space-x-2">
+      <div
+        class="fb-share-button"
+        data-href="https://fundtherebuild.com"
+        data-layout="button_count"
+        data-size="large">
+        <a
+          target="_blank"
+          href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Ffundtherebuild.com%2F&amp;src=sdkpreparse"
+          class="fb-xfbml-parse-ignore">
+          Share
+        </a>
+      </div>
+      <div>
+        <a
+          href="https://twitter.com/share?ref_src=twsrc%5Etfw"
+          class="twitter-share-button"
+          data-size="large"
+          data-url="https://fund-the-rebuild.now.sh"
+          data-show-count="true">
+          Tweet
+        </a>
+      </div>
+    </div>
+
   </div>
 
   <div class="mt-4 flex flex-col divide-y sm:space-y-8">
